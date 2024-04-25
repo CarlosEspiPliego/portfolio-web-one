7488d3d0da017f073a0d91037334041087e3febc
Carlos Espinoza <2022378002@uteq.edu.mx>
2024-04-25 11:59:28 -0600

    Avance con proyectos

--- scripts/skills.js
+++ scripts/skills.js
@@ -1,5 +1,5 @@
 const activeSideBtn = (content) => {
-    var sections = ['home__side-btn', 'sobre__side-btn', 'hobbies__side-btn', 'formacion__side-btn', 'skills__side-btn', 'contacto__side-btn'];
+    var sections = ['home__side-btn', 'sobre__side-btn', 'hobbies__side-btn', 'formacion__side-btn', 'projects__side-btn', 'skills__side-btn', 'contacto__side-btn'];
     
     // Ocultar todas las secciones
     sections.forEach(function (section) {
@@ -16,6 +16,7 @@ window.addEventListener('scroll', function () {
     var sobre = document.getElementById('sobre').offsetTop;
     var hobbies = document.getElementById('hobbies').offsetTop;
     var formacion = document.getElementById('formacion').offsetTop;
+    var projects = document.getElementById('projects').offsetTop;
     var skills = document.getElementById('skills').offsetTop;
     var contacto = document.getElementById('contacto').offsetTop;
 
@@ -27,8 +28,10 @@ window.addEventListener('scroll', function () {
         activeSideBtn('hobbies__side-btn');
     } else if (scroll >= formacion && scroll < skills) {
         activeSideBtn('formacion__side-btn');
-    } else if (scroll >= skills && scroll < contacto) {
+    } else if (scroll >= skills && scroll < projects ) {
         activeSideBtn('skills__side-btn');
+    } else if (scroll >= projects && scroll < contacto) {
+        activeSideBtn('projects__side-btn');
     } else if (scroll >= contacto) {
         activeSideBtn('contacto__side-btn');
     }
